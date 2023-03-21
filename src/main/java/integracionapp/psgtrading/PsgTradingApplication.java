c225fe6fb8ac6d1c8618fa5f144307db05e36736
Marcela Bustamante <julbustamante@uade.edu.ar>
2023-03-21 20:49:58 -0300

    initial commit

---
+++ src/main/java/integracionapp/psgtrading/PsgTradingApplication.java
@@ -0,0 +1,13 @@
+package integracionapp.psgtrading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsgTradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsgTradingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/integracionapp/psgtrading/PsgTradingApplicationTests.java
@@ -0,0 +1,13 @@
+package integracionapp.psgtrading;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsgTradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
