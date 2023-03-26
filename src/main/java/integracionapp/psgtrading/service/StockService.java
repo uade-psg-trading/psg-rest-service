5906338fade1cae02a5f754e4cd78e024a14dd20
Marcela Bustamante <julbustamante@uade.edu.ar>
2023-03-25 21:16:12 -0300

    yahoo finance api

--- src/main/java/integracionapp/psgtrading/service/StockService.java
+++ src/main/java/integracionapp/psgtrading/service/StockService.java
@@ -3,14 +3,20 @@ package integracionapp.psgtrading.service;
 import integracionapp.psgtrading.model.StockWrapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 @Service
 public class StockService {
+
+    private final RefreshService refreshService;
     public StockWrapper findStock(final String ticker) {
         try {
             return new StockWrapper(YahooFinance.get(ticker));
@@ -20,8 +26,19 @@ public class StockService {
 
         return null;
     }
-
     public BigDecimal findPrice(final StockWrapper stock) throws IOException {
-        return stock.getStock().getQuote(true).getPrice();
+        return stock.getStock().getQuote(refreshService.shouldRefresh(stock)).getPrice();
+    }
+
+    public List<StockWrapper> findStocks(final List<String> tickers) {
+        return tickers.stream().map(this::findStock).filter(Objects::nonNull).collect(Collectors.toList());
+    }
+
+    public BigDecimal findLastChangePercent(final StockWrapper stock) throws IOException {
+        return stock.getStock().getQuote(refreshService.shouldRefresh(stock)).getChangeInPercent();
+    }
+
+    public BigDecimal findChangeFrom200MeanPerccent(final StockWrapper stock) throws IOException {
+        return stock.getStock().getQuote(refreshService.shouldRefresh(stock)).getChangeFromAvg200();
     }
 }
