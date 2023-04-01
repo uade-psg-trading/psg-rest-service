152a1d853d9a271f38c781e78f5b355c7bcba4ed
Marcela Bustamante <julbustamante@uade.edu.ar>
2023-03-31 21:12:09 -0300

    delete codesmell

--- src/test/java/integracionapp/psgtrading/service/UserServiceTest.java
+++ src/test/java/integracionapp/psgtrading/service/UserServiceTest.java
@@ -13,7 +13,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import static org.mockito.Mockito.*;
 
 @SpringBootTest
-public class UserServiceTest {
+ class UserServiceTest {
 
     @Autowired
     private UserService userService;
