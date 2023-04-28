025f6f7b8874aff0e5b092399c9ca15e5dc5a3af
Migue <shevchuk.miguel@gmail.com>
2023-04-27 21:58:31 -0300

    Cambio del nombre para que fer no llore

--- src/main/java/integracionapp/psgtrading/controller/CoinController.java
+++ src/main/java/integracionapp/psgtrading/controller/CoinController.java
@@ -2,24 +2,17 @@ package integracionapp.psgtrading.controller;
 
 import integracionapp.psgtrading.dto.GenericDTO;
 import integracionapp.psgtrading.dto.coinMarket.response.CoinDTO;
-import integracionapp.psgtrading.dto.response.Yield;
-import integracionapp.psgtrading.model.Transaction;
 import integracionapp.psgtrading.service.NewStockService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @AllArgsConstructor
 @RequestMapping("/coin")
-public class MonedaController {
+public class CoinController {
 
 
     private final NewStockService newStockService;
