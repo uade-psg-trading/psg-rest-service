c199855f8d91caa98f75cd417ea2e2dc3bd83ccf
emanuelbrea <ebrea@uade.edu.ar>
2023-05-16 08:12:37 -0300

    Activo scheduler para buscar precios cada 10 min.

--- src/main/java/integracionapp/psgtrading/PsgTradingApplication.java
+++ src/main/java/integracionapp/psgtrading/PsgTradingApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-//@EnableScheduling
+@EnableScheduling
 public class PsgTradingApplication {
 
 	public static void main(String[] args) {
